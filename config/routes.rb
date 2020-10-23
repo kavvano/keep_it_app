e648f1fbab85b394faeaf302d7c296690d04ab6b
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-23 23:31:57 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
   resources :tweets do
-    resources :comment, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
 end
