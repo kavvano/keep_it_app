b2b2c3ae9089a2d998474e29e44a7a6f924325f6
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-24 17:35:59 +0900

    comment function setting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    if comment.invalid?
+      flash[:alert] = comment.errors.full_messages
+    else
+      comment.save
+    end
+    redirect_to tweet_path(comment[:tweet_id])
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
