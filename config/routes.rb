f785bf61fbae63521e5e5d2039ba7852456754d8
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-25 13:44:18 +0900

    set up routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "tweets#index"
   resources :tweets do
     resources :comments, only: [:create, :destroy]
+    resources :keep_it_ups, only: [:create, :destroy]
   end
 end
