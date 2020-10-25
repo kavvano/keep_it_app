6f89eae57878c89a7dbbd0ea84fad037f7597b6f
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-25 22:26:29 +0900

    keep it ups controller settings

--- app/controllers/keep_it_ups_controller.rb
+++ app/controllers/keep_it_ups_controller.rb
@@ -1,2 +1,22 @@
 class KeepItUpsController < ApplicationController
+  def create
+    unless KeepItUp.exists?(user_id: current_user.id, tweet_id: params[:tweet_id])
+      KeepItUp.create(keep_it_up_params)
+    end
+    redirect_to tweet_path(params[:tweet_id])
+  end
+
+  def destroy
+    keep_it_up = KeepItUp.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    if keep_it_up.present?
+      keep_it_up.destroy
+    end
+    redirect_to tweet_path(params[:tweet_id])
+  end
+
+  private
+
+  def keep_it_up_params
+    params.permit(:tweet_id).merge(user_id: current_user.id)
+  end
 end
