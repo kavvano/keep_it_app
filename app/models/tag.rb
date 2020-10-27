c59db55aac977d1dd2fb86277b04064ba9219a32
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-27 21:24:24 +0900

    fixed validation and association

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :tweet_tags
   has_many :tweets, through: :tweet_tags
+
+  validates :name, uniqueness: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,11 +3,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :keep_it_ups
   has_many :comments, dependent: :destroy
-  has_many :tweet_tags
+  has_many :tweet_tags, dependent: :destroy
   has_many :tags, through: :tweet_tags
-
-  with_options presence: true do
-    validates :user
-    validates :text, length: { maximum: 255 }
-  end
 end
