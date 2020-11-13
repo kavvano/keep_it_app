3dbd8f7e0bc10924c123c4c2f9e34864ce256140
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-11-13 14:44:03 +0900

    write test code for keep it up model

--- spec/models/keep_it_up_spec.rb
+++ spec/models/keep_it_up_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe KeepItUp, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @keep_it_up = FactoryBot.build(:keep_it_up)
+  end
+
+  describe 'いいね' do
+    context 'いいねできるとき' do
+      it 'userとtweetが存在すればいいねできる' do
+        expect(@keep_it_up).to be_valid
+      end
+    end
+
+    context 'いいねできないとき' do
+      it 'ユーザーが紐づいていないといいねできない' do
+        @keep_it_up.user = nil
+        @keep_it_up.valid?
+        expect(@keep_it_up.errors.full_messages).to include('Userを入力してください')
+      end
+      it 'ツイートが紐づいていないといいねできない' do
+        @keep_it_up.tweet = nil
+        @keep_it_up.valid?
+        expect(@keep_it_up.errors.full_messages).to include('Tweetを入力してください')
+      end
+    end
+  end
 end
